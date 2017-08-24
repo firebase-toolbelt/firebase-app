d46b643ee091045f092037c5b11470403b23ccbb
Georges Boris <georgesboris@gmail.com>
2017-08-24 10:46:06 -0300

    fix strict equal comparison

--- src/rules/utils/getPathsWithRules.js
+++ src/rules/utils/getPathsWithRules.js
@@ -2,7 +2,7 @@ module.exports = function getPathsWithRules(paths, ruleKey) {
 	let pathWithRules = 0;
 
 	Object.keys(paths).forEach(pathId => {
-    if (pathId == ruleKey) {
+    if (pathId === ruleKey) {
       pathWithRules = pathWithRules + 1;
     }
   });
